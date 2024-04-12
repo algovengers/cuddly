83820eb129309e3ae858c0004a5d652787b52412
subharthi75 <subharthi75@gmail.com>
2024-04-13 02:16:54 +0530

    redux user fix

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -5,7 +5,7 @@ import {
   registerUser,
 } from "../controllers/user.controller";
 // import { chatWithAi } from "controllers/chat.controller";
-import { expalorePets } from "../controllers/pet.controller";
+import { explorePets } from "../controllers/pet.controller";
 import { Router } from "express";
 
 const router = Router();
@@ -14,6 +14,6 @@ router.route("/signup").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(logoutUser);
 router.route("/signinwithgoogle").post(googleSingup);
-router.route("/explore").get(explorePets)
+router.route("/explore").get(explorePets);
 
 export default router;
