b200f1489b7a7cde4fcc21dc7049f4177f335690
Aftab Alam <128703784+CodeItAftab@users.noreply.github.com>
2024-04-20 15:49:43 +0530

    few changes (#79)

--- client/src/components/PetCard.tsx
+++ client/src/components/PetCard.tsx
@@ -1,4 +1,9 @@
 
+
+
+
+
+
 function PetCard({
   image,
   name,
@@ -8,8 +13,7 @@ function PetCard({
 }: Record<string, string | null>) {
   return (
     <div
-
-      className="border-[0.1px] border-[#00000033] border-[1px] w-50 sm:w-52 rounded-2xl overflow-hidden relative  cursor-default hover:shadow-[0_0_4px_2px_rgba(0,0,0,0.1),0_0_2px_0_rgba(0,0,0,0.1)] hover:border-[#0000001f] transition-all"
+      className="border-[0.1px] border-[#00000033] border-[1px] w-[224px] sm:w-52 rounded-2xl overflow-hidden relative  cursor-default hover:shadow-[0_0_4px_2px_rgba(0,0,0,0.1),0_0_2px_0_rgba(0,0,0,0.1)] hover:border-[#0000001f] transition-all"
       style={{ height: "260px" }}
     >
       <div className="overflow-hidden" style={{ height: "140px" }}>
@@ -25,14 +29,12 @@ function PetCard({
           <div>{name.toUpperCase() ||"Tom"}</div>
         </div>
         <div className="flex flex-row gap-2 mb-2 text-xs">
-
           <div>{gender.toUpperCase() || "Male"}</div>
           <div>•</div>
           <div>{breed.toLocaleUpperCase() || "Healthy"}</div>
         </div>
         {/* <div>{description}</div> */}
         <div className="absolute bottom-2 flex items-center gap-2 text-sm"><span className="material-symbols-outlined text-base">location_on</span>{location.toUpperCase()}</div>
-
       </div>
     </div>
   );
--- client/src/pages/PetData.tsx
+++ client/src/pages/PetData.tsx
@@ -1,117 +1,77 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
-import Loading from "./Loading";
-import axios from "axios";
-import PetPageCard from "@/components/PetPageCard";
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import Loading from './Loading';
+import axios from 'axios';
+import PetPageCard from '@/components/PetPageCard';
+import Navbar from '@/components/Navbar';
 
 function PetData() {
-    const [data, setData] = useState<Record<string, string> | null>();
-    const [loading, setLoading] = useState(true);
-    const id = useParams().id;
-    const getData = async () => {
+  
+    const [data,setData] = useState<Record<string,string> | null>();
+    const [loading,setLoading] = useState(true);
+    const id = useParams().id
+    const getData = async() =>{
         try {
-            const data = await axios.get(
-                `${import.meta.env.VITE_BACKEND_PATH}/api/pet/${id}`
-            );
-            setData(data?.data?.data);
-            setLoading(false);
-            console.log(data.data.data);
+            const data = await axios.get(`${import.meta.env.VITE_BACKEND_PATH}/api/pet/${id}`)
+            setData(data?.data?.data)
+            setLoading(false)
+            console.log(data.data.data)
         } catch (error) {
-            console.log("there is a error in showing petData ", error);
+            // console.log(error);
         }
-    };
-    useEffect(() => {
-        console.log(id);
-        getData();
-    }, []);
-    if (loading) {
-        return <Loading />;
     }
+    useEffect(()=>{
+        console.log(id)
+        getData()
+    },[])
+    if(loading){
+        return <Loading />
+    }
+  return (
+    <div className=' flex flex-col justify-center items-center bg-slate-50 min-h-screen relative
+    '>
+        <div className='w-full absolute top-0 left-0'>
 
-    return (
-        <div
-            className=" flex justify-center bg-slate-50 min-h-screen
-    "
-        >
-            <div className="p-4">
-                <div className=" max-w-8xl">
-                    <div className="grid grid-cols-3 gap-4">
-                        <div className=" col-span-2 flex flex-col">
-                            <div className=" bg-gray-900 flex justify-center items-center h-96 w-[600px] ">
-                                <img
-                                    src={data!.Image}
-                                    alt=""
-                                    className=" max-w-full max-h-full "
-                                />
-                            </div>
-                            <div className="text-4xl mt-4">
-                                Hey Myself {data.name}
-                            </div>
-                            <div className="mt-8">
-                                <div className=" text-2xl">About Me</div>
-                                <div className="grid grid-cols-2 gap-y-2 text-lg mt-3">
-                                    <div>I am a {data.type}</div>
-                                    <div>
-                                        <span className="font-bold mr-1">
-                                            {" "}
-                                            Personality:{" "}
-                                        </span>{" "}
-                                        {data.personality}
-                                    </div>
-                                    <div>
-                                        <span className="font-bold mr-1">
-                                            {" "}
-                                            Breed:{" "}
-                                        </span>{" "}
-                                        {data.breed}
-                                    </div>
-                                    <div>
-                                        <span className="font-bold mr-1">
-                                            {" "}
-                                            Color:{" "}
-                                        </span>
-                                        {data.color}
-                                    </div>
-                                    <div>
-                                        <span className="font-bold mr-1">
-                                            {" "}
-                                            Gender:{" "}
-                                        </span>
-                                        {data.gender}
-                                    </div>
-                                    <div>
-                                        <span className="font-bold mr-1">
-                                            {" "}
-                                            Weight:{" "}
-                                        </span>
-                                        {data.weight}kg
-                                    </div>
-                                    <div>
-                                        <span className="font-bold mr-1">
-                                            {" "}
-                                            City:{" "}
-                                        </span>
-                                        {data.city}
-                                    </div>
-                                    <div>
-                                        <span className="font-bold mr-1">
-                                            {" "}
-                                            Age:{" "}
-                                        </span>
-                                        {data.age} year
-                                    </div>
-                                </div>
-                            </div>
+        <Navbar/>
+        </div>
+        <div className='p-4 mt-14'>
+
+        <div className=' xsm:max-w-4/6'>
+            <div className='grid grid-cols-1 md:gap-4 gap-y-4'>
+                <div className=' col-span-2 flex md:flex-row flex-col gap-6 '>
+                    <div className=' bg-gray-900 flex justify-center items-center h-full md:m-0 m-auto md:w-[400px] w-[320px] rounded-lg '>
+                    <img src={data!.Image} alt="" className=' max-w-full max-h-full ' />
+                    </div>
+                    <div className="pet-details ml-2">
+
+                        <div className='text-4xl mt-4'>Hey Myself {data.name}</div>
+                        <div className='mt-8'>
+                            <div className=' text-lg font-light tracking-widest	'>ABOUT ME</div>
+                        <div className='grid grid-cols-2 gap-6 md:text-lg text-base mt-3'>
+                                <div>I am a {data.type}</div>
+                                <div><span className='font-medium mr-1'> Personality: </span>  {data.personality}</div>
+                                <div><span className='font-medium mr-1'> Breed: </span> {data.breed}</div>
+                                <div><span className='font-medium mr-1'> Color: </span>{data.color}</div>
+                                <div><span className='font-medium mr-1'> Gender: </span>{data.gender}</div>
+                                <div><span className='font-medium mr-1'> Weight: </span>{data.weight}kg</div>
+                                <div><span className='font-medium mr-1'> City: </span>{data.city}</div>
+                                <div><span className='font-medium mr-1'> Age: </span>{data.age} year</div>
+
                         </div>
-                        <div className=" col-span-1">
-                            <PetPageCard owner={data.owner} name={data.name} />
-
                         </div>
                     </div>
                 </div>
+                <div className=' col-span-1'>
+                    <PetPageCard owner={data.owner} name={data.name} />
+                </div>
             </div>
         </div>
-    );
+        </div>
+    </div>
+  )
 }
 
-export default PetData;
+export default PetData
+
+
+
